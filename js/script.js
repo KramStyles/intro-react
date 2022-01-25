ea1f31dd13579154f76a1b940d24ee2832b45bd7
Michael Jamie <kramstyles@outlook.com>
2022-01-25 23:35:07 +0100

    rendered with functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+function Navbar() {
+    return(
+        <h1>Nothing</h1>
+    )
+}
+
+function MainContent(){
+    return(<h1>I am learning React</h1>);
+}
+
 ReactDOM.render(<h1>Welcome and Hello</h1>, document.getElementById('root'));
+ReactDOM.render(<div><Navbar /><MainContent /></div>, document.getElementById('root'));
